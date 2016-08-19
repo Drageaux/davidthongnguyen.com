cad4bf5c7a70d324254ad142a5c24120c09b1ff6
David Thong Nguyen <thnguyen2808@gmail.com>
2016-08-19 19:00:26 -0400

    Fading between Who subsections

--- custom/js/main.js
+++ custom/js/main.js
@@ -27,3 +27,25 @@ function animateProgress() {
         }
     }
 }
+
+function sectionFade(direction) {
+    if (direction == "left") {
+        if (!$("#who-2").hasClass("hidden")) {
+            $("#who-2").transition("fade left");
+            setTimeout(function () {
+                $("#who-1").transition("fade right");
+            }, 500);
+            $("#stats-left-btn").addClass("disabled");
+            $("#stats-right-btn").removeClass("disabled");
+        }
+    } else if (direction == "right") {
+        if (!$("#who-1").hasClass("hidden")) {
+            $("#who-1").transition("fade right");
+            setTimeout(function () {
+                $("#who-2").transition("fade left");
+            }, 500);
+            $("#stats-right-btn").addClass("disabled");
+            $("#stats-left-btn").removeClass("disabled");
+        }
+    }
+}
