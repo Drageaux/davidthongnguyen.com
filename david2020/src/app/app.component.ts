7515a294b691efbf9eaedd3094b5291ab8683b0b
David Thong Nguyen <dev@davidtn.com>
2020-01-02 13:15:26 -0800

    Still deciding on bg color; added some neumorphic elements

--- david2020/src/app/app.component.ts
+++ david2020/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFileDownload } from '@fortawesome/pro-duotone-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'david2020';
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+
+  faFileDownload = faFileDownload;
 }
--- david2020/src/app/app.module.ts
+++ david2020/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
