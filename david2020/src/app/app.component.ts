cdaf4d654f380372b6bb7580355882993154408d
David Thong Nguyen <thnguyen2808@gmail.com>
2020-01-03 21:54:40 -0800

    Making the well equal to the intro card

--- david2020/src/app/app.component.ts
+++ david2020/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faDownload, faEye } from '@fortawesome/pro-solid-svg-icons';
+import { faUser } from '@fortawesome/pro-regular-svg-icons';
+// import { faUser } from '@fortawesome/pro-duotone-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,8 @@ export class AppComponent {
   faLinkedin = faLinkedin;
   faDownload = faDownload;
   faEye = faEye;
+
+  faUser = faUser;
 
   particleNum = 50;
 }
