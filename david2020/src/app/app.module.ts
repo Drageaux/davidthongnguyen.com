37da2f9bc7541aef35f60ea5677790dab7766d5c
David Thong Nguyen <thnguyen2808@gmail.com>
2020-01-18 15:25:15 -0800

    Refactor into components

--- david2020/src/app/app.module.ts
+++ david2020/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutComponent } from './sections/about/about.component';
+import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AboutComponent, TestimonialsComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
