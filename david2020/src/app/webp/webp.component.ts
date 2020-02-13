1ca4fa178642bb002dc7a2a28c7a5e641cf3e1da
David Thong Nguyen <dev@davidtn.com>
2020-02-13 13:24:02 -0800

    Reusable webp component

---
+++ david2020/src/app/webp/webp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webp',
+  templateUrl: './webp.component.html',
+  styleUrls: ['./webp.component.scss']
+})
+export class WebpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
