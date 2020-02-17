fda17abbe0d66047a7da6a27d8b5466a94c50a7d
David Thong Nguyen <dev@davidtn.com>
2020-02-17 12:35:09 -0800

    Trying to work with png and jpg but not working

--- david2020/webp.js
+++ david2020/webp.js
@@ -9,22 +9,12 @@ var imagemin = require('imagemin'), // The imagemin module.
 var outputFolders = ['./src/assets', './src/assets/project-thumbs'];
 var breakpoints = [576, 768, 992, 1200];
 
-// imagemin([`${folder}/*.{jpg,png}`], folder, {
-//   use: [
-//     webp({
-//       quality: 65 // Quality setting from 0 to 100
-//     })
-//   ]
-// }).then(img => {
-//   console.log(`image: ${img.sourcePath} output to ${img.destinationPath}`);
-// });
-
-const generateWebp = async (folder, subFolder, width) => {
-  const webpImages = await imagemin([`${folder}/*.{jpg,png}`], {
+const generateResponsiveImages = async (folder, subFolder, width) => {
+  const images = await imagemin([`${folder}/*.{jpg,png}`], {
     destination: `${folder}/${subFolder}`,
     plugins: [
       webp({
-        quality: 100, // Quality setting from 0 to 100,
+        quality: 65, // Quality setting from 0 to 100,
         resize: {
           width,
           height: 0
@@ -39,7 +29,8 @@ const generateWebp = async (folder, subFolder, width) => {
     ]
   }).catch(console.error);
   // console.log(`image: ${img.sourcePath} output to ${img.destinationPath}`);
-  webpImages.forEach(({ sourcePath, destinationPath }) => {
+  console.log(images);
+  images.forEach(({ sourcePath, destinationPath }) => {
     console.log(
       `sourcePath: ${sourcePath}\ndestinationPath: ${destinationPath}\n-------`
     );
@@ -53,35 +44,9 @@ var projectThumbResolutions = [
   { subFolder: 'lg', width: 450 }
 ];
 projectThumbResolutions.forEach(res => {
-  generateWebp(`${assetsFolder}/project-thumbs`, res.subFolder, res.width);
+  generateResponsiveImages(
+    `${assetsFolder}/project-thumbs`,
+    res.subFolder,
+    res.width
+  );
 });
-// default/pure-mobile < 350px = 200px
-// xs/big-mobile 350-576px = 450px
-// sm 576+ = 250px
-// md 768+ = 325px
-// lg 992+ = 410px
-// xl 1200+ = 260px
-
-// (async () => {
-//   const pngfiles = await imagemin([PNGImages], {
-//     destination: outputFolder,
-//     plugins: [
-//       webp({
-//         quality: 65 // Quality setting from 0 to 100
-//       })
-//     ]
-//   });
-// console.log(pngfiles);
-// // })();
-
-// async () => {
-//   const jpgFiles = imagemin([JPEGImages], {
-//     destination: outputFolder,
-//     plugins: [
-//       webp({
-//         quality: 65 // Quality setting from 0 to 100
-//       })
-//     ]
-//   });
-//   console.log(jpgFiles);
-// };
