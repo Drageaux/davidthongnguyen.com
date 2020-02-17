21cae7ffe87cc94c4cfd532d1497d7dad34a1c42
David Thong Nguyen <dev@davidtn.com>
2020-02-17 10:25:24 -0800

    Working on optimizing images

--- david2020/webp.js
+++ david2020/webp.js
@@ -1,29 +1,60 @@
 var imagemin = require('imagemin'), // The imagemin module.
   webp = require('imagemin-webp'), // imagemin's WebP plugin.
-  outputFolder = './src/assets/', // Output folder
+  assetsFolder = './src/assets', // Output folder
   PNGImages = './src/assets/*.png', // PNG images
   JPEGImages = './src/assets/*.jpg'; // JPEG images
 
-(async () => {
-  const pngfiles = await imagemin([PNGImages], {
-    destination: outputFolder,
-    plugins: [
-      webp({
-        quality: 65 // Quality setting from 0 to 100
-      })
-    ]
-  });
-  console.log(pngfiles);
-})();
+var outputFolders = ['./src/assets', './src/assets/project-thumbs'];
+var breakpoints = [576, 768, 992, 1200];
 
-async () => {
-  const jpgFiles = imagemin([JPEGImages], {
-    destination: outputFolder,
+// imagemin([`${folder}/*.{jpg,png}`], folder, {
+//   use: [
+//     webp({
+//       quality: 65 // Quality setting from 0 to 100
+//     })
+//   ]
+// }).then(img => {
+//   console.log(`image: ${img.sourcePath} output to ${img.destinationPath}`);
+// });
+
+const generateWebp = async (target, dest) => {
+  const webpImages = await imagemin([target], {
+    destination: dest,
     plugins: [
       webp({
         quality: 65 // Quality setting from 0 to 100
       })
     ]
-  });
-  console.log(jpgFiles);
+  }).catch(console.error);
+  // console.log(`image: ${img.sourcePath} output to ${img.destinationPath}`);
+  console.log(webpImages);
 };
+
+generateWebp(
+  `${assetsFolder}/project-thumbs/*.{jpg,png}`,
+  `${assetsFolder}/project-thumbs/`
+).then();
+
+// (async () => {
+//   const pngfiles = await imagemin([PNGImages], {
+//     destination: outputFolder,
+//     plugins: [
+//       webp({
+//         quality: 65 // Quality setting from 0 to 100
+//       })
+//     ]
+//   });
+// console.log(pngfiles);
+// // })();
+
+// async () => {
+//   const jpgFiles = imagemin([JPEGImages], {
+//     destination: outputFolder,
+//     plugins: [
+//       webp({
+//         quality: 65 // Quality setting from 0 to 100
+//       })
+//     ]
+//   });
+//   console.log(jpgFiles);
+// };
