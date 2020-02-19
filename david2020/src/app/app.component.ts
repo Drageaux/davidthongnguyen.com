551eaacbbced2605bf238a5077a9123cb76b02bd
David Thong Nguyen <dev@davidtn.com>
2020-02-19 13:32:07 -0800

    Working on detecting page changes for sections

--- david2020/src/app/app.component.ts
+++ david2020/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { distinctUntilChanged } from 'rxjs/operators';
+
+// Declare ga as ambient
+declare var ga;
+declare var $;
 
 @Component({
   selector: 'app-root',
@@ -24,5 +31,34 @@ export class AppComponent {
     ? 'https://cdn130.picsart.com/240548021044202.jpg?r1024x1024'
     : 'https://cdn130.picsart.com/240548021044202.jpg?r1024x1024';
 
-  constructor() {}
+  coverPhotoSpecs = {
+    widthList: [400, 600, 800],
+    sizes: '(max-width: 992px) 90vw, 33vw'
+  };
+
+  constructor(private router: Router) {
+    $('body').scrollspy({ target: '', offset: 100 });
+    // Using Rx's built in `distinctUntilChanged ` feature to handle url change c/o @dloomb's answer
+    this.router.events
+      .pipe(
+        distinctUntilChanged((previous: any, current: any) => {
+          // Subscribe to any `NavigationEnd` events where the url has changed
+          if (current instanceof NavigationEnd) {
+            return previous.url === current.url;
+          }
+          return true;
+        })
+      )
+      .subscribe((x: any) => {
+        console.log(x.url);
+        ga('set', 'page', x.url);
+        ga('send', 'pageview');
+      });
+
+    $(window).on('activate.bs.scrollspy', function(e) {
+      console.log(e.target);
+
+      history.replaceState({}, '', $('.nav-item .active').attr('href'));
+    });
+  }
 }
